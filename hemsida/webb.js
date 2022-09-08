b10fd5b139866cce5e0559cf1d88d62b29d593a2
abbannulv <69853424+abbannulv@users.noreply.github.com>
2022-09-08 13:25:00 +0200

    hemsidan, rätt så stor uppdatering

---
+++ hemsida/webb.js
@@ -0,0 +1,9 @@
+
+
+// function knapp () {
+//     alert("jag kan släcka din heta eld med min vätska ;)")
+// }
+
+let btnPrimary = document.querySelector('#button')
+
+btnPrimary.addEventListener('click', () => btnPrimary.getElementsByClassName.backgroundColor = '#337ab7')
