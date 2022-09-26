acf31bd662a5173d2567a115e94761ff02e33bb2
AbbLavinBamarni <lavin.bamarni@abbindustrigymnasium.se>
2022-09-26 17:02:54 +0200

    fixed fan and water buttons

--- hemsida/index.js
+++ hemsida/index.js
@@ -6,4 +6,21 @@
 
 let btnPrimary = document.querySelector('#button')
 
+let watering = false
+let flakt = false
+
 btnPrimary.addEventListener('click', () => btnPrimary.getElementsByClassName.backgroundColor = '#337ab7')
+
+function vattna () {
+    watering = !watering
+    console.log(watering)
+
+    fetch("settings?water=" + watering)
+}
+
+function fan () {
+    flakt = !flakt
+    console.log(flakt)
+
+    fetch("settings?fan=" + (flakt ? 255 : 0))
+}
